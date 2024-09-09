4e3c4b9f58e856af5cb20eeea94e7cd29f11cea6
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-09-09 10:55:59 +0530

    Create Divider.jsx

---
+++ Divider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Divider = () => {
+  return (
+    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/1086eee71a8fe6314428f402aa4a8198548c23b276b244159ee7db1e327f9411?placeholderIfAbsent=true&apiKey=78e38421974242f189e269164ea93cd8" alt="" className="object-contain mt-36 w-full aspect-[1000] max-w-[1348px] max-md:mt-10 max-md:max-w-full" />
+  );
+};
+
+export default Divider;
